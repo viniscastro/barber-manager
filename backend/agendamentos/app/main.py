fe24cdc6706a849994b029b3c78c0cb18838bd35
ArthurJoseV <arthurjosevinicius@hotmail.com>
2026-05-15 23:39:12 -0300

    Criação de uma tela de login cenográfica, adição de imagens e mudanças nas formataçõesde texto

--- backend/agendamentos/app/main.py
+++ backend/agendamentos/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware 
 from sqlalchemy.orm import Session
 from . import models, schemas, database
 
