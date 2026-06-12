a8bce697888a49100df97c711891a14034493366
ArthurJoseV <arthurjosevinicius@hotmail.com>
2026-06-12 17:10:12 -0300

    Adição de API Gateway Nginx

--- backend/servicos/app/main.py
+++ backend/servicos/app/main.py
@@ -1,3 +1,6 @@
+import os
+import json
+import redis
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
@@ -7,6 +10,7 @@ from . import models, schemas, database
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI(title="Barber Manager - API de serviços")
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], 
@@ -14,9 +18,29 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+redis_url = os.getenv("REDIS_URL", "redis://redis_cache:6379/0")
+redis_client = redis.Redis.from_url(redis_url)
+
 @app.get("/servicos/", response_model=List[schemas.ServicoResponse])
 def listar_servicos(db: Session = Depends(database.get_db)):
-    return db.query(models.Servico).all()
+    servicos_em_cache = redis_client.get("servicos_lista")
+    
+    if servicos_em_cache:
+        print("⚡ Buscou do CACHE (Redis)!")
+        return json.loads(servicos_em_cache)
+
+    print("🐘 Buscou do BANCO DE DADOS (PostgreSQL)!")
+    servicos_db = db.query(models.Servico).all()
+    servicos_lista = []
+    for servico in servicos_db:
+        servicos_lista.append({
+            "id": servico.id,
+            "nome": servico.nome,
+            "preco": float(servico.preco)
+        })
+    redis_client.setex("servicos_lista", 3600, json.dumps(servicos_lista))
+    
+    return servicos_db
 
 @app.post("/servicos/", response_model=schemas.ServicoResponse)
 def criar_servico(servico: schemas.ServicoCreate, db: Session = Depends(database.get_db)):
@@ -24,6 +48,9 @@ def criar_servico(servico: schemas.ServicoCreate, db: Session = Depends(database
     db.add(novo_servico)
     db.commit()
     db.refresh(novo_servico)
+
+    redis_client.delete("servicos_lista")
+    
     return novo_servico
 
 @app.put("/servicos/{servico_id}", response_model=schemas.ServicoResponse)
@@ -36,6 +63,8 @@ def atualizar_servico(servico_id: int, servico_atualizado: schemas.ServicoCreate
     servico_db.preco = servico_atualizado.preco
     db.commit()
     db.refresh(servico_db)
+    redis_client.delete("servicos_lista")
+    
     return servico_db
 
 @app.delete("/servicos/{servico_id}")
@@ -46,4 +75,6 @@ def deletar_servico(servico_id: int, db: Session = Depends(database.get_db)):
     
     db.delete(servico_db)
     db.commit()
+    redis_client.delete("servicos_lista")
+    
     return {"mensagem": "Serviço deletado com sucesso"}
